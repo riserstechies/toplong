024f3f2230689d92c9e078d13461061efb29f1d0
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-18 02:38:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/QIXING-Womens-Sleeve-Neck-Blouse/dp/B07J596ZHK/ref=gbps_tit_m-9_53c0_30520258?smid=A37BIDZPY74ACV&pf_rd_p=806222ac-2f87-4ede-a31c-ac60f36f53c0&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=JM9WJBFWAMFSQXZ08TKV">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.amazon.com/QIXING-Womens-Sleeve-Neck-Blouse/dp/B07J596ZHK/ref=gbps_tit_m-9_53c0_30520258?smid=A37BIDZPY74ACV&pf_rd_p=806222ac-2f87-4ede-a31c-ac60f36f53c0&pf_rd_s=merchandised-search-9&pf_rd_t=101&pf_rd_i=15529609011&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=JM9WJBFWAMFSQXZ08TKV"
         </script>
         <title>Page Redirection</title>
     </head>
